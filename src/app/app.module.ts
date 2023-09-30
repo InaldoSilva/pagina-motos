169db97008eb4829f679d4ff82dd1fd80a9d4004
Inaldo Silva <104039452+InaldoSilva@users.noreply.github.com>
2023-09-30 01:08:10 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardComponent } from './components/card/card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    MenuBarComponent,
+    CardComponent,
+    SmallCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
